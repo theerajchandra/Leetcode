14619c0d634ed008ecaf58dffcc7610b58db62d3
Theeraj Chandra <tchandra@uwaterloo.ca>
2025-12-02 12:21:52 -0500

    Time: 231 ms (26.84%), Space: 66.8 MB (69.9%) - LeetHub

---
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.py
@@ -0,0 +1,51 @@
+class StockPrice:
+
+    def __init__(self):
+        self.timestamps = {}
+        self.max_time = 0
+        self.min_heap = []
+        self.max_heap = []
+        
+
+    def update(self, timestamp: int, price: int) -> None:
+        self.timestamps[timestamp] = price
+        self.max_time = max(self.max_time, timestamp)
+
+        heapq.heappush(self.min_heap, (price, timestamp))
+        heapq.heappush(self.max_heap, (-price, timestamp))
+        
+
+    def current(self) -> int:
+        return self.timestamps[self.max_time]
+        
+
+    def maximum(self) -> int:
+        cur_price, timestamp = heapq.heappop(self.max_heap)
+
+        while -cur_price != self.timestamps[timestamp]:
+            cur_price, timestamp = heapq.heappop(self.max_heap)
+
+        heapq.heappush(self.max_heap, (cur_price, timestamp))
+
+        return -cur_price
+
+        
+
+    def minimum(self) -> int:
+        cur_price, timestamp = heapq.heappop(self.min_heap)
+
+        while cur_price != self.timestamps[timestamp]:
+            cur_price, timestamp = heapq.heappop(self.min_heap)
+
+        heapq.heappush(self.min_heap, (cur_price, timestamp))
+
+        return cur_price
+        
+
+
+# Your StockPrice object will be instantiated and called as such:
+# obj = StockPrice()
+# obj.update(timestamp,price)
+# param_2 = obj.current()
+# param_3 = obj.maximum()
+# param_4 = obj.minimum()
