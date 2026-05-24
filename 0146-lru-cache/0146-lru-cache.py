a95c22d09ec13ca9fa2825b4a46db3841e6845fe
Theeraj Chandra <tchandra@uwaterloo.ca>
2026-05-24 17:58:56 -0400

    Time: 117 ms (63.95%), Space: 76.7 MB (91.67%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,28 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1        
+        self.cache.move_to_end(key) # mark as recently used
+        return self.cache[key]
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)  # evict LRU (front of dict)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
